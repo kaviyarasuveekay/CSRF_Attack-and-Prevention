1c71a7d4758e9cabf1a0f61fc9d80d926a35b95a
Kaviyarasu V <84955527+kaviyarasuveekay@users.noreply.github.com>
2022-07-28 22:14:59 +0530

    Add files via upload

---
+++ Prevention/main1.php
@@ -0,0 +1,13 @@
+<?php
+include 'main.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="styles.css" rel="stylesheet">
+	<title>main page</title>
+</head>
+<body>
+	<h1 style="position: absolute;top: 300px;left: 400px;">Welcome to main page</h1>
+</body>
+</html>
